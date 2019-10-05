2f2729d64de92296a86065b3d9760a9373aff6f4
fuzetsu <fuzetsu@users.noreply.github.com>
2019-10-04 23:01:07 -0400

    move root state logic to index.js

--- src/cmp/todo-list.js
+++ src/cmp/todo-list.js
@@ -1,12 +1,8 @@
-import { useReducer, useEffect, useState, m } from '../lib/hooks.js'
-import { safeParse } from '../lib/util.js'
+import { useState, m } from '../lib/hooks.js'
 import Todo, { newTodo } from './todo.js'
 
-const savedTodos = safeParse(localStorage.todoV1, [])
-
-export default function TodoList() {
-  const [state, update] = useReducer(mergerino, { todos: savedTodos, draft: newTodo('') })
-  useEffect(() => (localStorage.todoV1 = JSON.stringify(state.todos)), [state.todos])
+export default function TodoList({ state, update }) {
+  const { draft, todos = [] } = state
 
   const [focusIndex, setFocusIndex] = useState(-1)
   const keyHandler = e => {
@@ -21,13 +17,13 @@ export default function TodoList() {
       setFocusIndex(focusIndex + 1)
     } else if (e.key === 'ArrowDown' || (!e.shiftKey && e.key === 'Tab')) {
       e.preventDefault()
-      setFocusIndex(Math.min(focusIndex + 1, state.todos.length - 1))
+      setFocusIndex(Math.min(focusIndex + 1, todos.length - 1))
     } else if (e.key === 'ArrowUp' || (e.shiftKey && e.key === 'Tab')) {
       e.preventDefault()
       setFocusIndex(Math.max(-1, focusIndex - 1))
     } else if (e.key === 'Backspace' && focusIndex > -1 && document.activeElement.value === '') {
       update({ todos: { [focusIndex]: undefined } })
-      const newLength = state.todos.length - 1
+      const newLength = todos.length - 1
       if (focusIndex > 0) setFocusIndex(focusIndex - 1)
       else if (newLength < 1) setFocusIndex(-1)
       else if (focusIndex > newLength - 1) setFocusIndex(newLength - 1)
@@ -39,13 +35,13 @@ export default function TodoList() {
     { onkeydown: keyHandler },
     m(Todo, {
       draft: true,
-      todo: state.draft,
+      todo: draft,
       update,
       focused: focusIndex === -1,
       local: up => update({ draft: up }),
       onclick: () => setFocusIndex(-1)
     }),
-    state.todos.map((todo, idx) =>
+    todos.map((todo, idx) =>
       m(Todo, {
         key: todo.id,
         todo,
@@ -55,12 +51,6 @@ export default function TodoList() {
         onclick: () => setFocusIndex(idx)
       })
     ),
-    m(
-      '' + z`mt 8;ml 30`,
-      state.todos.filter(x => x.done).length,
-      ' done, ',
-      state.todos.length,
-      ' total'
-    )
+    m('' + z`mt 8;ml 30`, todos.filter(x => x.done).length, ' done, ', todos.length, ' total')
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import TodoList from './cmp/todo-list.js'
-import { m } from './lib/hooks.js'
+import { m, useReducer, useEffect } from './lib/hooks.js'
+import { safeParse } from './lib/util.js'
+import { newTodo } from './cmp/todo.js'
 
 z.helper({
   size: (x, y = x) => `h ${x}; w ${y}`,
@@ -11,8 +13,17 @@ z.global`
   fs 16
 `
 
+const savedTodos = safeParse(localStorage.todoV1, [])
+
 const App = () => {
-  return m('main' + z`m auto; w 400`, m('h1' + z`m 10;ml 25`, 'Todo'), m(TodoList))
+  const [state, update] = useReducer(mergerino, { todos: savedTodos, draft: newTodo('') })
+  useEffect(() => (localStorage.todoV1 = JSON.stringify(state.todos)), [state.todos])
+
+  return m(
+    'main' + z`m auto; w 400`,
+    m('h1' + z`m 10;ml 25`, 'Todo'),
+    m(TodoList, { state, update })
+  )
 }
 
 preact.render(m(App), document.body)
